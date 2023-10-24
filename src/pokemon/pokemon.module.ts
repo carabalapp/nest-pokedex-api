6831b18acc84b5dff0b3cea26758940a35cbde69
Adrian Caraballo <carabalapp@gmail.com>
2023-10-24 01:44:51 -0400

    [add] - config modules with imports and exports dependencies

--- src/pokemon/pokemon.module.ts
+++ src/pokemon/pokemon.module.ts
@@ -12,6 +12,17 @@ import { Pokemon, PokemonSchema } from './entities/pokemon.entity';
       name: Pokemon.name,
       schema: PokemonSchema
     }])
+  ],
+  exports: [
+    // El MongooseModule podemos exportarlo de esta manera nada mas
+    // No haria falta exportarlo de esta manera 
+    // MongooseModule.forFeature([{
+    //   name: Pokemon.name,
+    //   schema: PokemonSchema
+    // }])
+
+    //  Debido a que ya tiene la configuracion que le colocamos en imports
+    MongooseModule
   ]
 })
 export class PokemonModule {}
--- src/seed/seed.service.ts
+++ src/seed/seed.service.ts
@@ -1,11 +1,19 @@
 import { Injectable } from '@nestjs/common';
 import axios, { AxiosInstance } from 'axios';
 import { PokeResponse } from './interfaces/poke-response.interface';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { Pokemon } from 'src/pokemon/entities/pokemon.entity';
 
 @Injectable()
 export class SeedService {
 
     private readonly axios: AxiosInstance = axios
+
+    constructor(
+        @InjectModel(Pokemon.name)
+        private readonly pokemonModel: Model<Pokemon>
+    ) { }
 
     async executeSeed() {
         // Aca estabamos obteniendo un error de circular-json el cual nos daba este error en postman
@@ -21,18 +29,20 @@ export class SeedService {
 
         // Lo que ocasionaba que me diera el error de circular JSON debido a que es un JSON muy grande y no sabe a cual propiedad acceder
 
-        const { data } = await this.axios.get<PokeResponse>('https://pokeapi.co/api/v2/pokemon?limit=1')
+        const { data } = await this.axios.get<PokeResponse>('https://pokeapi.co/api/v2/pokemon?limit=10')
 
         // Aca nosotros queremos guardar el "no" del pokemon en nuestra semilla, pero estamos obetiendo solo el name y el url
         // Pero en el url tenemos el no, solo debemos saber manejar la data para obtenerlo
-
-        data.results.forEach(({name, url}) => {
+        let noRows = 0
+        data.results.forEach(async ({ name, url }) => {
             const segments = url.split('/')
             // Aqui nos va arrojar un string pero necesitamops que sea de tipo number
             // Para eso le colocamos un '+' adelante y ya lo transforma en un tipo number
             const no = +segments[segments.length - 2]
-        })
 
-        return data.results
+            noRows += 1;
+            await this.pokemonModel.create({ name, no })
+        })
+        return `Seed executed, no of row inserts ${noRows}`
     }
 }
